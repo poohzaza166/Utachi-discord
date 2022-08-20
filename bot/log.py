cf1d8b9908930e17c3a3b189f9683379d2764a7f
poohzaza166 <poohzaza166@gmail.com>
2022-08-21 02:25:38 +0700

    modified:   bot/log.py fix formating error

--- bot/log.py
+++ bot/log.py
@@ -3,17 +3,15 @@ from . import fileio
 
 botconfig = fileio.botconfig
 
-formatss = ("%(asctime)s;%(levelname)s;%(message)s",
-                              "%H:%M:%S %d-%m-%Y")
 
 if botconfig['bot_setting']['loglevel'] == "debug":
-    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',encoding='utf-8',level=logging.DEBUG, format=formatss)
+    logging.basicConfig(filename='app.log', filemode='w',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',encoding='utf-8',level=logging.DEBUG)
 
 elif botconfig['bot_setting']['log_level'] == "info":
-    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',encoding='utf-8',level=logging.INFO, format=formatss)
+    logging.basicConfig(filename='app.log', filemode='w',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',encoding='utf-8',level=logging.INFO)
 
 elif botconfig['bot_setting']['log_level'] == "error":
-    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',encoding='utf-8',level=logging.ERROR, format=formatss)
+    logging.basicConfig(filename='app.log', filemode='w',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',encoding='utf-8',level=logging.ERROR)
 
 
 logs = logging.getLogger(__name__)
