d54927334760f8a05e4def71c8374255c79763ad
poohzaza166 <poohzaza166@gmail.com>
2022-08-21 02:15:35 +0700

    re add typing animation 	modified:   bot/log.py 	modified:   bot/__main__.py 	modified:   bot/extendtions/profile/__main__.py 	modified:   bot/log.py

--- bot/log.py
+++ bot/log.py
@@ -1,4 +1,18 @@
 import logging
+from . import fileio
 
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',encoding='utf-8',level=logging.INFO)
+botconfig = fileio.botconfig
+
+if botconfig['bot_setting']['loglevel'] == "debug":
+    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',encoding='utf-8',level=logging.DEBUG)
+
+elif botconfig['bot_setting']['log_level'] == "info":
+    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',encoding='utf-8',level=logging.INFO)
+
+elif botconfig['bot_setting']['log_level'] == "error":
+    logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',encoding='utf-8',level=logging.ERROR)
+
+logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
+                              "%H:%M:%S %d-%m-%Y")
+
 logs = logging.getLogger(__name__)
