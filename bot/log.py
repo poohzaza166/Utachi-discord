b9a6af45d48066fe93e02a569c999d7641c4200a
poohzaza166 <poohzaza166@gmail.com>
2022-08-24 07:57:00 +0700

    modified:   bot/log.py

--- bot/log.py
+++ bot/log.py
@@ -7,10 +7,10 @@ botconfig = fileio.botconfig
 if botconfig['bot_setting']['loglevel'] == "debug":
     logging.basicConfig(filename='config/app.log', filemode='w',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',encoding='utf-8',level=logging.DEBUG)
 
-elif botconfig['bot_setting']['log_level'] == "info":
+elif botconfig['bot_setting']['loglevel'] == "info":
     logging.basicConfig(filename='config/app.log', filemode='w',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',encoding='utf-8',level=logging.INFO)
 
-elif botconfig['bot_setting']['log_level'] == "error":
+elif botconfig['bot_setting']['loglevel'] == "error":
     logging.basicConfig(filename='config/app.log', filemode='w',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',encoding='utf-8',level=logging.ERROR)
 
 
